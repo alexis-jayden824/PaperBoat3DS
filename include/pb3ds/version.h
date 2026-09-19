e2ba067e4147276f29a15771043501a0ebe80b17
Jayden Alexis <157524274+alexis-jayden824@users.noreply.github.com>
2026-09-19 13:16:35 -0400

    chore: advance development build to M4

--- include/pb3ds/version.h
+++ include/pb3ds/version.h
@@ -1,8 +1,8 @@
 #pragma once
 
 #define PB3DS_PROJECT_NAME "PaperBoat3DS"
-#define PB3DS_VERSION "0.2.0-m2"
-#define PB3DS_ROADMAP_STAGE "M2 diagnostics foundation"
+#define PB3DS_VERSION "0.4.0-m4"
+#define PB3DS_ROADMAP_STAGE "M4 compatibility core"
 
 #ifndef PB3DS_BUILD_SHA
 #define PB3DS_BUILD_SHA "unknown"
