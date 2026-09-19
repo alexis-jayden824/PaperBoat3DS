e4c6fd3ed4adcd04cf9793545dcabec9fbd9fb71
Jayden Alexis <157524274+alexis-jayden824@users.noreply.github.com>
2026-09-19 12:59:58 -0400

    chore: mark native bootstrap as M1

--- include/pb3ds/version.h
+++ include/pb3ds/version.h
@@ -1,6 +1,5 @@
 #pragma once
 
 #define PB3DS_PROJECT_NAME "PaperBoat3DS"
-#define PB3DS_VERSION "0.0.1-m0"
-#define PB3DS_ROADMAP_STAGE "M0 toolchain bootstrap"
-
+#define PB3DS_VERSION "0.1.0-m1"
+#define PB3DS_ROADMAP_STAGE "M1 native app bootstrap"
